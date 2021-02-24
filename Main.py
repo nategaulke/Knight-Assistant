d095fd68e3960b8b5537ca9c1cd126b1e9fea07a
= <=>
2021-02-23 20:03:28 -0500

    Adds a model function to main in order to demonstrate how to populate the !help command.

--- Main.py
+++ Main.py
@@ -9,7 +9,16 @@ builtins.bot = bot
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
-hi
+
+"""
+The below function is a model for how we should define all command functions. Use the "help" decorator and put a description of the command there.
+Whenever a user types "!help", a list of all command descriptions will show.
+~Conroy
+"""
+@bot.command(help = "This command is an example of how to contribute information to the \"!help\" command.")
+async def model_command(ctx):
+    await ctx.channel.send("placeholder.")
+
 @bot.event
 async def on_ready():
     print(f"{bot.user.name} has entered the Bounce House!")
