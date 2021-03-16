60b7a94fb2a11fba65698e0f2f7e826eb32d35d5
= <=>
2021-03-16 11:37:39 -0400

    Added a cog the does simple math calculations.

---
+++ cogs/calc.py
@@ -0,0 +1,56 @@
+#This script was written by Conroy
+
+import discord
+from discord.ext import commands
+
+#inherit from commands.Cog
+class Calc(commands.Cog):
+
+    def __init__(self, bot):
+
+        #reference the bot object from Main.py
+        self.bot = bot
+
+
+    @commands.command(help = "This command will do simple math. Type a simple math expression with only 1 operator after \"!calc\" and the bot will solve it for you!")
+    async def calc(self, ctx, x, operator, y):
+
+        channel = ctx.message.channel
+        tempx = float(x)
+        tempy = float(y)
+        response = ""
+
+        #check for all basic math calculations
+        if operator == "+" or operator.lower() == "plus":
+
+            response = f"The answer is {(tempx + tempy)}."
+
+        elif operator == "-" or operator.lower() == "minus":
+
+            response = f"The answer is {(tempx - tempy)}."
+
+        elif operator == "/" or operator.lower() == "divide":
+
+            response = f"The answer is {(tempx / tempy)}."
+
+        elif operator == "*" or operator.lower() == "multiply":
+
+            response = f"The answer is {(tempx * tempy)}."
+
+        elif operator == "%" or operator.lower() == "mod" or operator.lower() == "modulo" or operator.lower() == "modulos":
+
+            response = f"The answer is {(tempx % tempy)}."
+
+        elif operator == "^" or operator.lower() == "power":
+
+            response = f"The answer is {(tempx ** tempy)}."
+
+        else:
+
+            response = "Sorry, I do not recognize that oeprator yet."
+        
+        await channel.send(response)
+
+#this function connects this cog (via the Calc class) to the bot object
+def setup(bot):
+    bot.add_cog(Calc(bot))
