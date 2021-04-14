1ca71676a5dd8e38d7b36fe8676ecb07a95202c9
Nathanael Gaulke <nate.gaulke@knights.ucf.edu>
2021-04-14 19:45:35 -0400

    allow user to find next break and cleans up help descriptions

--- cogs/Events.py
+++ cogs/Events.py
@@ -8,7 +8,7 @@ class Events(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=['addevent'], help = "Adds event to the server's calendar")
+    @commands.command(aliases=['addevent'], help = "Adds event to the server's calendar.")
     async def add_event(self, ctx, name = None, date = None, time = None):
         if ctx.author == bot.user:
             return
@@ -18,10 +18,10 @@ class Events(commands.Cog):
         if date == None:
             await ctx.send('Did you mean to add a task instead? Otherwise please redo command with a date')
             return
-        
+
         # create a date object for storing the information, must use strftime when translating date to string to print to user
         date = dt.datetime.strptime(date, '%B %d, %Y')
-        
+
         if time == None:
             events.append((name, date))
             embed = discord.Embed(title = 'Your event has been added to ' + ctx.message.guild.name, color = discord.Color(0xFFFF00))
@@ -37,8 +37,8 @@ class Events(commands.Cog):
             embed.set_footer(text = ctx.author.display_name, icon_url = ctx.author.avatar_url)
             await ctx.send(embed=embed)
         return
-    
-    @commands.command(aliases=['events', 'calendar'], help = "Shows the events on the server's calendar")
+
+    @commands.command(aliases=['events', 'calendar'], help = "Shows the events on the server's calendar.")
     async def show_events(self, ctx):
         if ctx.author == bot.user:
             return
@@ -51,7 +51,7 @@ class Events(commands.Cog):
         events.sort(key = lambda x: x[1])
         embed = discord.Embed(title = 'Events on ' + ctx.message.guild.name, color = discord.Color(0xFFFF00), timestamp = ctx.message.created_at)
         embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/a/a2/Calendar_12.png")
-        
+
         # Some events have a time so we first try to add that field in. If an IndexError pops up because event[2] (the time)
         # does not exist, then we make an exception and instead add the name and date only
         for event in events:
@@ -61,8 +61,8 @@ class Events(commands.Cog):
                 embed.add_field(name = event[0], value = event[1].strftime('%B %d, %Y'), inline=False)
         await ctx.send(embed=embed)
         return
-    
-    @commands.command(aliases=['delevent', 'delete_event', 'delevents'], help = "Deletes an event with the given name or all events")
+
+    @commands.command(aliases=['delevent', 'delete_event', 'delevents'], help = "Deletes an event with the given name or all events.")
     async def del_event(self, ctx, name = None):
         if ctx.author == bot.user:
             return
