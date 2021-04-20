0644b3787cef780cd9f620775ddb4edfd4ab01ce
Nathanael Gaulke <nate.gaulke@knights.ucf.edu>
2021-04-20 10:48:42 -0400

    Integrate MongoDB into the events feature of the bot

--- cogs/Events.py
+++ cogs/Events.py
@@ -1,8 +1,13 @@
+import datetime as dt
 import discord
 from discord.ext import commands
-import datetime as dt
+from dotenv import load_dotenv
+import os
+from pymongo import MongoClient
 
-events = []
+
+load_dotenv()
+MONGO_URL = os.getenv('MONGO_URL')
 
 class Events(commands.Cog):
     def __init__(self, bot):
@@ -19,20 +24,33 @@ class Events(commands.Cog):
             await ctx.send('Did you mean to add a task instead? Otherwise please redo command with a date')
             return
         
-        # create a date object for storing the information, must use strftime when translating date to string to print to user
+        # Create a datetime object for storing the date
         date = dt.datetime.strptime(date, '%B %d, %Y')
-        
+
+        # Connect to MongoDB and to collection
+        client = MongoClient(MONGO_URL)
+        db = client["Database"]
+        collection = db["Events"]
+
         if time == None:
-            events.append((name, date))
+            # Create Document & add to the collection
+            event = {'Time': date, 'Name': name, 'UserID': ctx.author.display_name, 'Guild': ctx.message.guild.name}
+            collection.insert_one(event)
             embed = discord.Embed(title = 'Your event has been added to ' + ctx.message.guild.name, color = discord.Color(0xFFFF00))
-            embed.add_field(name = events[-1][0], value = events[-1][1].strftime('%B %d, %Y'), inline=False)
+            embed.add_field(name = name, value = date.strftime('%B %d, %Y'), inline=False)
             embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/a/a2/Calendar_12.png")
             embed.set_footer(text = ctx.author.display_name, icon_url = ctx.author.avatar_url)
             await ctx.send(embed=embed)
         else:
-            events.append((name, date, time))
+            # Concatentate date with the specific time of the event
+            time = dt.datetime.strptime(time, "%I:%M %p").time()
+            date = dt.datetime.combine(date, time)
+
+            # Create Document & add to the collection
+            event = {'Time': date, 'Name': name, 'UserID': ctx.author.display_name, 'Guild': ctx.message.guild.name}
+            collection.insert_one(event)
             embed = discord.Embed(title = 'Your event has been added to ' + ctx.message.guild.name, color = discord.Color(0xFFFF00))
-            embed.add_field(name = events[-1][0], value = events[-1][1].strftime('%B %d, %Y') + " at " + events[-1][2], inline=False)
+            embed.add_field(name = name, value = date.strftime('%B %d, %Y at %I:%M %p'), inline=False)
             embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/a/a2/Calendar_12.png")
             embed.set_footer(text = ctx.author.display_name, icon_url = ctx.author.avatar_url)
             await ctx.send(embed=embed)
@@ -42,23 +60,25 @@ class Events(commands.Cog):
     async def show_events(self, ctx):
         if ctx.author == bot.user:
             return
-        if not events:
+        
+        # Connect to MongoDB and to collection
+        client = MongoClient(MONGO_URL)
+        db = client["Database"]
+        collection = db["Events"]
+
+        # Check if there are any guild documents in the collection beforehand
+        if collection.find({'Guild': ctx.message.guild.name}).count() == 0:
             embed = discord.Embed(title = 'No Events are on ' + ctx.message.guild.name, color = discord.Color(0xFFFF00), timestamp = ctx.message.created_at)
             embed.add_field(name = 'To add events use !addevent and separate name, date, and time using "" ', value = 'Example: !addevent "name" "date" "time(optional)"',inline=False)
             embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/a/a2/Calendar_12.png")
             await ctx.send(embed=embed)
             return
-        events.sort(key = lambda x: x[1])
+        
         embed = discord.Embed(title = 'Events on ' + ctx.message.guild.name, color = discord.Color(0xFFFF00), timestamp = ctx.message.created_at)
         embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/a/a2/Calendar_12.png")
-        
-        # Some events have a time so we first try to add that field in. If an IndexError pops up because event[2] (the time)
-        # does not exist, then we make an exception and instead add the name and date only
-        for event in events:
-            try:
-                embed.add_field(name = event[0], value = event[1].strftime('%B %d, %Y') + ' at ' + event[2], inline=False)
-            except IndexError:
-                embed.add_field(name = event[0], value = event[1].strftime('%B %d, %Y'), inline=False)
+        # If there are events, print them sorted by their date/time
+        for event in collection.find({'Guild': ctx.message.guild.name}).sort("Time"):
+            embed.add_field(name = event["Name"], value = event["Time"].strftime('%B %d, %Y at %I:%M %p'), inline=False)
         await ctx.send(embed=embed)
         return
     
@@ -66,33 +86,45 @@ class Events(commands.Cog):
     async def del_event(self, ctx, name = None):
         if ctx.author == bot.user:
             return
-        if not events:
+
+        # Connect to MongoDB and to collection
+        client = MongoClient(MONGO_URL)
+        db = client["Database"]
+        collection = db["Events"]
+
+        # Check if there are any guild documents in the collection beforehand
+        if collection.find({'Guild': ctx.message.guild.name}).count() == 0:
             embed = discord.Embed(title = 'No Events are on ' + ctx.message.guild.name, color = discord.Color(0xFFFF00), timestamp = ctx.message.created_at)
             embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/a/a2/Calendar_12.png")
             await ctx.send(embed=embed)
             return
+
         if name == None:
             ctx.send('Please redo command with the name of the event you would like to delete')
             return
+
+        # If user says "all" then all documents from the guild will be deleted from the collection        
         if name.lower() == "all":
-            events.clear()
+            collection.delete_many({'Guild': ctx.message.guild.name})
             embed = discord.Embed(title = 'All events have been deleted from ' + ctx.message.guild.name, color = discord.Color(0xFFFF00), timestamp = ctx.message.created_at)
             embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/a/a2/Calendar_12.png")
             embed.set_footer(text = ctx.author.display_name, icon_url = ctx.author.avatar_url)
             await ctx.send(embed=embed)
             return
-        for event in events:
-            if name.lower() == event[0].lower():
+        
+        # If user specifies a name, then collection is searched for the unique name
+        for event in collection.find({'Guild': ctx.message.guild.name}):
+            if name.lower() == event["Name"].lower():
                 embed = discord.Embed(title = 'Your event has been deleted from ' + ctx.message.guild.name, color = discord.Color(0xFFFF00))
-                try:
-                    embed.add_field(name = event[0], value = event[1].strftime('%B %d, %Y') + ' at ' + event[2], inline=False)
-                except IndexError:
-                    embed.add_field(name = event[0], value = event[1].strftime('%B %d, %Y'), inline=False)
+                embed.add_field(name = event["Name"], value = event["Time"].strftime('%B %d, %Y at %I:%M %p'), inline=False)
                 embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/a/a2/Calendar_12.png")
                 embed.set_footer(text = ctx.author.display_name, icon_url = ctx.author.avatar_url)
                 await ctx.send(embed=embed)
-                events.remove(event)
+                
+                # Once the name is found, it is deleted from the collection
+                collection.delete_one({'Time': event["Time"], 'Name': event["Name"], 'UserID': event["UserID"], 'Guild': event["Guild"]})
                 return
+
         embed = discord.Embed(title = 'Event is not listed in ' + ctx.message.guild.name, color = discord.Color(0xFFFF00))
         embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/a/a2/Calendar_12.png")
         await ctx.send(embed=embed)
